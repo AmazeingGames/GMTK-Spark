645bb8e7665e00dc1c0e5111ea69034f7c7e99c6
AmazeingGames <AmazeingGameDesign@gmail.com>
2024-08-27 23:28:06 -0400

    Removed Old Assets
    Deleted old scripts and assets from the spark game idea

--- GMTK-Spark/Assets/Spark/Scripts/Channel.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class Channel
-{
-    public enum Signal { Null, Alpha, Bravo, Charlie, Delta, Echo, Foxtrot, Golf, Hotel, India, Juliett, Kilo, Lima, Mike, November, Oscar, Papa, Quebec, Romeo, Sierra, Tango, Uniform, Victor, Whiskey, XRay, Yankee, Zulu }
-}
